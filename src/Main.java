ee392c5a1bd657917ff1d42960922cd6cc8a1bdc
Ekaterina Pavlova <sone4ka1414@gmail.com>
2023-03-12 19:06:34 +0300

    miles calculation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int price = 21576;
+        int oneMile = 20;
+
+        int bonus = price / oneMile;
+
+        System.out.println("Начислено миль: " + bonus);
+
     }
 }
